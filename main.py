093a06dfba9a6bf5e48fdacf7479012ee8e0ae08
pomyc <romult@ukr.net>
2025-04-23 21:24:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from bot_logic import handle_message
+import os
 
 app = Flask(__name__)
 
@@ -7,3 +8,7 @@ app = Flask(__name__)
 def webhook():
     data = request.get_json()
     return handle_message(data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
